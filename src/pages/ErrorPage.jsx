65f7c999e0707497484885e47306970fd8677b82
syedmdanial <syedmdanial.dev@gmail.com>
2024-02-06 02:54:12 +0800

    updated error page stylings

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
 import img from "../assets/not-found.svg";
 
 const ErrorPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="ErrorPage">
-      <img src={img} alt="not found" />
-      <h3>Ohh!</h3>
-      <p>We can't seem to find page you are looking for</p>
-      <button className="btn btn-solid" onClick={() => navigate("/")}>
-        Go to Home
-      </button>
-    </div>
+    <Wrapper>
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Ohh!</h3>
+        <p>We can't seem to find page you are looking for</p>
+        <button className="btn main-btn" onClick={() => navigate("/")}>
+          Go to Home
+        </button>
+      </div>
+    </Wrapper>
   );
 };
 
